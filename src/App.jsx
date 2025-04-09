54d398fc68c0a629e382b4fc1e74c60e5890cb70
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-04-08 21:25:47 -0600

    Project Initialization and Tailwind Configuration

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <main className="bg-gray-400">
+      <h1 className="text-3xl font-bold text-red-500 text-center">Hello Tailwind v.4</h1>
+    </main>
+  )
+}
+
+export default App
