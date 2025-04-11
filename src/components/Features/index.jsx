c67efef1bb671390ff8f4f03d4431f0522b29e94
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-04-10 19:37:22 -0600

    Features Data

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-import image1 from "../../assets/images/illustration-features-tab-1.svg"
+import React, { useState } from "react";
+import { featuresData } from "../utils/FeaturesData";
+
 
 const Features = () => {
+
+  const [activeData, setActiveData] = useState(featuresData[0]);
+
+  const handleActiveData = (data) => {
+    setActiveData(data)
+  }
+
   return (
     <section className="mt-10 p-7 overflow-x-hidden">
       <h2 className="text-Blue-950 font-bold text-center text-2xl">Features</h2>
@@ -11,18 +19,25 @@ const Features = () => {
 
       <div>
         <ul className="flex flex-col items-center w-full mt-10 gap-7 border-t-2 border-gray-300 pt-4">
-          <li className="border-b-2 border-gray-300 w-full text-center pb-4">Simple Bookmarking</li>
-          <li className="border-b-2 border-gray-300 w-full text-center pb-4">Speedy Searching</li>
-          <li className="border-b-2 border-gray-300 w-full text-center pb-4">Easy Sharing</li>
+          {
+            featuresData.map(feature => (
+              <li key={feature.id}
+                className="
+           relative border-b-2 border-gray-300 w-full text-center pb-4" onClick={() => handleActiveData(feature)}>
+                {feature.title}
+                <span className={`${activeData.title == feature.title ? 'block w-40 h-2 rounded-md bg-red-500 bottom-0 left-1/2 transform -translate-x-1/2 absolute' : ''}`}></span>
+              </li>
+            ))
+          }
         </ul>
 
         <div className="mt-16">
           <div className="mb-18 relative">
             <div className="w-[350px] h-52 bg-Blue-600 absolute -z-10 -left-20 -bottom-9 rounded-r-[6rem]"></div>
-            <img src={image1} alt="image1" />
+            <img src={activeData.image} alt={activeData.title} />
           </div>
-          <h3 className="text-center text-Blue-950 font-bold text-2xl">Bookmark in one click</h3>
-          <p className="text-center text-gray-400 mt-5 text-[.95rem]">Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.</p>
+          <h3 className="text-center text-Blue-950 font-bold text-2xl">{activeData.subTitle}</h3>
+          <p className="text-center text-gray-400 mt-5 text-[.95rem]">{activeData.info}</p>
         </div>
       </div>
 
@@ -32,4 +47,4 @@ const Features = () => {
 
 export { Features };
 
-//  after:content-[''] after:block after:w-2.5 after:h-2.5 after:bg-red-500
+//  
---
+++ src/components/utils/FeaturesData.js
@@ -0,0 +1,27 @@
+import ImgFeature1 from "../../assets/images/illustration-features-tab-1.svg"
+import ImgFeature2 from "../../assets/images/illustration-features-tab-2.svg"
+import ImgFeature3 from "../../assets/images/illustration-features-tab-3.svg"
+
+export const featuresData = [
+  {
+    id: 1,
+    title: "Simple Bookmarking",
+    image: ImgFeature1,
+    subTitle: "Bookmark in one click",
+    info: " Our aim is to make it quick and easy for yuo to access your favourite websites. Your booksmarks sync between your devices so you can access them on the go.",
+  },
+  {
+    id: 2,
+    title: "Speedy Searching",
+    image: ImgFeature2,
+    subTitle: "Intelligent search",
+    info: "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
+  },
+  {
+    id: 3,
+    title: "Easy Sharing",
+    image: ImgFeature3,
+    subTitle: "Share your bookmarks",
+    info: "Easily sjhare your bookmarks and collections with others. Create a shareable link that you can send at the click of a button. ",
+  },
+]
