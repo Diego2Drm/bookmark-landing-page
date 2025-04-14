6f3e14cd97138169c470a159aa788b60f84b5f9d
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-04-14 13:20:06 -0600

    Add desktop design for Login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -29,10 +29,10 @@ const Login = () => {
       <p className="text-Grey-50 text-sm font-light tracking-widest">
         35.000 + ALREADY JOINER
       </p>
-      <h4 className="text-Grey-50 text-xl font-bold text-center">Stay up-to-date whit what we´re doing</h4>
+      <h4 className="text-Grey-50 text-xl font-bold text-center lg:text-2xl lg:w-96 lg:px-4">Stay up-to-date whit what we´re doing</h4>
 
-      <form className="w-full flex flex-col gap-5 mt-2">
-        <div className={`${error ? 'bg-Red-400 p-2 rounded-md' : ''} relative`}>
+      <form className="w-full flex flex-col gap-5 mt-2 lg:w-1/2 lg:flex-row">
+        <div className={`${error ? 'bg-Red-400 p-2 rounded-md' : ''} relative lg:w-3/4`}>
           <input type="text" placeholder="Enter you email adrees" className="bg-Grey-50 p-3 w-full rounded-lg placeholder:text-gray-300 pr-10"
             value={email}
             onChange={handleChange}
@@ -45,7 +45,7 @@ const Login = () => {
             </>
           }
         </div>
-        <button className="bg-Red-400 text-Grey-50 font-semibold w-full p-3 rounded-lg"
+        <button className="bg-Red-400 text-Grey-50 font-semibold w-full p-3 rounded-lg lg:w-1/4"
           onClick={handleSubmit}
         >
           Contact Us
